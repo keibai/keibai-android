2ee5ebfb496faac190b7114de8c18d149dd7bb58
Eduard Torres Montiel <etorres404@gmail.com>
2017-11-03 11:05:07 +0100

    Added Home, Activities and Profile fragments. Implemented basic navigation between these three fragments. Changed main activity layout to RelativeLayout

---
+++ app/src/main/java/io/github/keibai/profile/ProfileFragment.java
@@ -0,0 +1,40 @@
+package io.github.keibai.profile;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.github.keibai.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ProfileFragment extends Fragment {
+
+
+    public ProfileFragment() {
+        // Required empty public constructor
+    }
+
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstance) {
+        super.onCreate(savedInstance);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+}
