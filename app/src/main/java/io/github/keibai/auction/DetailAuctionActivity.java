f7077fa1a6d25d5d7a1daba96baac4fda9400493
zurfyx <zurfyx@gmail.com>
2017-11-04 20:19:34 +0100

    view auction activity

---
+++ app/src/main/java/io/github/keibai/auction/DetailAuctionActivity.java
@@ -0,0 +1,15 @@
+package io.github.keibai.auction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.keibai.R;
+
+public class DetailAuctionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_auction);
+    }
+}
