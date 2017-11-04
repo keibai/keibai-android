d37e2be07914da52bf3f23ad9ff03696a16a1490
zurfyx <zurfyx@gmail.com>
2017-11-04 13:20:40 +0100

    view event back button

--- app/src/main/java/io/github/keibai/event/DetailEventActivity.java
+++ app/src/main/java/io/github/keibai/event/DetailEventActivity.java
@@ -1,10 +1,12 @@
 package io.github.keibai.event;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import io.github.keibai.R;
 
