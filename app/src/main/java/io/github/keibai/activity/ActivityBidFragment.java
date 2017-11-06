ed97340f7e94755b9af0af33aca7825f862f399d
Eduard Torres Montiel <etorres404@gmail.com>
2017-11-06 15:37:16 +0100

    Implementation of activities bid list

--- app/src/main/java/io/github/keibai/activity/ActivityBidFragment.java
+++ app/src/main/java/io/github/keibai/activity/ActivityBidFragment.java
@@ -5,8 +5,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import io.github.keibai.R;
+import io.github.keibai.auction.Transaction;
+import io.github.keibai.auction.TransactionAdapter;
 
 public class ActivityBidFragment extends Fragment{
 
@@ -22,7 +29,26 @@ public class ActivityBidFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_activities_bid, container, false);
+        View view = inflater.inflate(R.layout.fragment_activities_bid, container, false);
+        ListView listView = view.findViewById(R.id.activities_bid_list);
+
+        // TODO: Change in next sprint, transactions will be retrieved using the API
+        List<Transaction> transactions = new ArrayList<>();
+        Calendar calendar = Calendar.getInstance();
+
+        transactions.add(new Transaction("You", 10.31f,
+                calendar, "Auction1", Transaction.BID_MESSAGE));
+        transactions.add(new Transaction("You", 10.45f,
+                calendar, "Auction 2", Transaction.BID_MESSAGE));
+        transactions.add(new Transaction("You", 12.31f,
+                calendar, "Auction 3", Transaction.BID_MESSAGE));
+        transactions.add(new Transaction("You", 15.31f,
+                calendar, "Auction 4", Transaction.BID_MESSAGE));
+
+        TransactionAdapter transactionAdapter = new TransactionAdapter(getContext(), transactions);
+        listView.setAdapter(transactionAdapter);
+
+        return view;
     }
 
 }
