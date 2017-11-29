bb4050e97ab9424f3504aeb264aeeecb036660ee
Eduard Torres Montiel <etorres404@gmail.com>
2017-11-29 19:03:58 +0100

    Parsed EditText fields of sign in activity and created attempt user

--- app/src/main/java/io/github/keibai/activities/signin/SignInActivity.java
+++ app/src/main/java/io/github/keibai/activities/signin/SignInActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import io.github.keibai.activities.MainActivity;
 import io.github.keibai.R;
 import io.github.keibai.SaveSharedPreference;
+import io.github.keibai.models.User;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -22,11 +25,19 @@ public class SignInActivity extends AppCompatActivity {
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // TODO: Change in next sprint, sign in logic here
-                SaveSharedPreference.setUserId(getApplicationContext(), 1);
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
+                EditText etEmail = findViewById(R.id.sign_in_edit_text_email);
+                EditText etPassword = findViewById(R.id.sign_in_edit_text_password);
+
+                User attemptUser = new User();
+                attemptUser.email = etEmail.getText().toString();
+                attemptUser.password = etPassword.getText().toString();
+
+                // TODO: Connection with the server here
+                Toast.makeText(getApplicationContext(), attemptUser.toString(), Toast.LENGTH_LONG).show();
+//                SaveSharedPreference.setUserId(getApplicationContext(), 1);
+//                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+//                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+//                startActivity(intent);
             }
         });
     }
