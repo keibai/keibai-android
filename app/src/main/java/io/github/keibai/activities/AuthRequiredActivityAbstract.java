77108b9902e4363f0fb2630c526fb2eb46e72ccb
zurfyx <zurfyx@gmail.com>
2017-12-03 19:36:13 +0100

    auth required activity

---
+++ app/src/main/java/io/github/keibai/activities/AuthRequiredActivityAbstract.java
@@ -0,0 +1,71 @@
+package io.github.keibai.activities;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import java.io.IOException;
+
+import io.github.keibai.SaveSharedPreference;
+import io.github.keibai.activities.welcome.WelcomeActivity;
+import io.github.keibai.http.Http;
+import io.github.keibai.http.HttpCallback;
+import io.github.keibai.http.HttpUrl;
+import io.github.keibai.models.User;
+import io.github.keibai.models.meta.Error;
+import io.github.keibai.runnable.RunnableToast;
+import okhttp3.Call;
+
+public abstract class AuthRequiredActivityAbstract extends AppCompatActivity {
+
+    /**
+     * Check if signed or move the user to the welcome page.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // Quick pre-check. If no user id saved we can assume the user is not authenticated.
+        if (SaveSharedPreference.getUserId(getApplicationContext()) == -1) {
+            redirectToWelcomePage();
+            return;
+        }
+
+        // Check signed in status remotely.
+        // @See remoteCheck()
+        new Http(getApplicationContext()).get(HttpUrl.userWhoami(), new HttpCallback<User>(User.class) {
+            @Override
+            public void onError(Error error) throws IOException {
+                runOnUiThread(new RunnableToast(getApplicationContext(), error.toString()));
+            }
+
+            @Override
+            public void onSuccess(User user) throws IOException {
+                remoteCheck(user);
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+                runOnUiThread(new RunnableToast(getApplicationContext(), e.toString()));
+            }
+        });
+    }
+
+    /**
+     * User session might have expired. If so, remove the userId from the Shared Storage and move
+     * the user to the welcome page.
+     * @param user
+     */
+    public void remoteCheck(User user) {
+        if (user.id == 0) {
+            SaveSharedPreference.setUserId(getApplicationContext(), -1);
+            redirectToWelcomePage();
+        }
+    }
+
+    public void redirectToWelcomePage() {
+        Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/io/github/keibai/activities/MainActivity.java
+++ app/src/main/java/io/github/keibai/activities/MainActivity.java
@@ -1,36 +1,22 @@
 package io.github.keibai.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
-import io.github.keibai.activities.welcome.WelcomeActivity;
 import io.github.keibai.R;
-import io.github.keibai.SaveSharedPreference;
 import io.github.keibai.activities.activity.ActivityFragment;
 import io.github.keibai.activities.home.HomeFragment;
 import io.github.keibai.activities.profile.ProfileFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AuthRequiredActivityAbstract {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        if (SaveSharedPreference.getUserId(getApplicationContext()) == -1) {
-            // Not signed in. Go to welcome screen.
-            Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
-            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-            return;
-        }
-
-        // Display main activity
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = findViewById(R.id.bottombar_main);
