be24f0f4a0d457343089fbb63b0130e23ad874f4
zurfyx <zurfyx@gmail.com>
2017-12-03 20:10:58 +0100

    HttpCallback handle unexpected server responses

--- app/src/main/java/io/github/keibai/http/HttpCallback.java
+++ app/src/main/java/io/github/keibai/http/HttpCallback.java
@@ -1,6 +1,7 @@
 package io.github.keibai.http;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 
 import java.io.IOException;
 
@@ -21,16 +22,49 @@ public abstract class HttpCallback<T> implements Callback {
     public void onResponse(Call call, Response response) throws IOException {
         ResponseBody responseBody = response.body();
         if (!response.isSuccessful()) {
-            Error error = new Gson().fromJson(responseBody.string(), Error.class);
-            onError(error);
+            Error error = fromJson(responseBody.string(), Error.class);
+            onError(error == null ? customError("Unexpected server response.") : error);
             return;
         }
 
-        T obj = new Gson().fromJson(responseBody.string(), className);
-        onSuccess(obj);
+        T obj = fromJson(responseBody.string(), className);
+        if (obj == null) {
+            onError(customError("Unexpected object response."));
+        } else {
+            onSuccess(obj);
+        }
     }
 
+    /**
+     * Always returns an Error object.
+     * The Error message will be taken from the server response, if possible.
+     * Otherwise a "Unexpected server response." will be returned in an Error object if the response
+     * code is not 200.
+     * If the response code is 200 and the object cannot be cast to whatever it was indicated, a
+     * "Unexpected object response." Error will be returned.
+     * @param error
+     * @throws IOException
+     */
     public abstract void onError(Error error) throws IOException;
 
+    /**
+     * Returns a T object, or null.
+     * @param response
+     * @throws IOException
+     */
     public abstract void onSuccess(T response) throws IOException;
+
+    private Error customError(String errorMessage) {
+        Error error = new Error();
+        error.error = errorMessage;
+        return error;
+    }
+
+    private <E> E fromJson(String content, Class<E> className) {
+        try {
+            return new Gson().fromJson(content, className);
+        } catch (JsonSyntaxException e) {
+            return null;
+        }
+    }
 }
