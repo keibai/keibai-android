6edd79c8d8bd7c3e57262cc11c57a488568d053c
zurfyx <zurfyx@gmail.com>
2017-12-03 13:08:11 +0100

    sign up validation

--- app/src/main/java/io/github/keibai/activities/signup/SignUpActivity.java
+++ app/src/main/java/io/github/keibai/activities/signup/SignUpActivity.java
@@ -3,6 +3,7 @@ package io.github.keibai.activities.signup;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,82 +12,80 @@ import android.widget.Toast;
 import java.io.IOException;
 
 import io.github.keibai.SaveSharedPreference;
-import io.github.keibai.BuildConfig;
 import io.github.keibai.activities.MainActivity;
 import io.github.keibai.R;
+import io.github.keibai.form.DefaultAwesomeValidation;
 import io.github.keibai.http.Http;
 import io.github.keibai.http.HttpCallback;
 import io.github.keibai.http.HttpUrl;
 import io.github.keibai.models.User;
 import io.github.keibai.models.meta.Error;
+import io.github.keibai.runnable.RunnableToast;
 import okhttp3.Call;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private DefaultAwesomeValidation validation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        validation = new DefaultAwesomeValidation(getApplicationContext());
+        validation.addValidation(this, R.id.edit_sign_up_name, "[a-zA-Z\\s]+", R.string.name_invalid);
+        validation.addValidation(this, R.id.edit_sign_up_last_name, "[a-zA-Z\\s]+", R.string.last_name_invalid);
+        validation.addValidation(this, R.id.edit_sign_up_email, Patterns.EMAIL_ADDRESS, R.string.email_invalid);
+        validation.addValidation(this, R.id.edit_sign_up_repeat_password, R.id.edit_sign_up_password, R.string.repeat_email_invalid);
+        validation.addValidation(this, R.id.edit_sign_up_password, ".{4,}", R.string.password_invalid);
+        validation.addValidation(this, R.id.edit_sign_up_repeat_password, R.id.edit_sign_up_password, R.string.password_invalid);
+
         Button signUpButton = findViewById(R.id.button_sign_up_submit);
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                EditText etName = findViewById(R.id.sign_up_edit_text_name);
-                EditText etLastName = findViewById(R.id.sign_up_edit_text_last_name);
-                EditText etEmail = findViewById(R.id.sign_up_edit_text_email);
-                EditText etRepeatEmail = findViewById(R.id.sign_up_edit_text_repeat_email);
-                EditText etPassword = findViewById(R.id.sign_up_edit_text_password);
-                EditText etRepeatPassword = findViewById(R.id.sign_up_edit_text_repeat_password);
+        signUpButton.setOnClickListener(new SignUp());
+    }
 
-                // Check email
-                String email = etEmail.getText().toString();
-                String repeatedEmail = etRepeatEmail.getText().toString();
-                if (!email.equals(repeatedEmail)) {
-                    Toast.makeText(getApplicationContext(), "Email not equals", Toast.LENGTH_SHORT).show();
-                    etEmail.setText("");
-                    etRepeatEmail.setText("");
-                    return;
-                }
+    public User userFromForm() {
+        EditText formName = findViewById(R.id.edit_sign_up_name);
+        EditText formLastName = findViewById(R.id.edit_sign_up_last_name);
+        EditText formEmail = findViewById(R.id.edit_sign_up_email);
+        EditText formPassword = findViewById(R.id.edit_sign_up_password);
 
-                // Check password
-                String password = etPassword.getText().toString();
-                String repeatedPassword = etRepeatPassword.getText().toString();
-                if (!password.equals(repeatedPassword)) {
-                    Toast.makeText(getApplicationContext(), "Password not equals", Toast.LENGTH_SHORT).show();
-                    etPassword.setText("");
-                    etRepeatPassword.setText("");
-                    return;
-                }
+        User user = new User();
+        user.name = formName.getText().toString();
+        user.lastName = formLastName.getText().toString();
+        user.email = formEmail.getText().toString();
+        user.password = formPassword.getText().toString();
 
-                // Others checks will be performed on the server side
-                User attemptUser = new User();
-                attemptUser.name = etName.getText().toString();
-                attemptUser.lastName = etLastName.getText().toString();
-                attemptUser.email = etEmail.getText().toString();
-                attemptUser.password = etPassword.getText().toString();
+        return user;
+    }
 
-                new Http(getApplicationContext()).post(HttpUrl.newUserUrl(), attemptUser, new HttpCallback<User>(User.class) {
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
-                    }
-
-                    @Override
-                    public void onError(Error error) throws IOException {
-                        System.out.println("this is an error");
-                        System.out.println(error);
-                    }
-
-                    @Override
-                    public void onSuccess(User response) throws IOException {
-                        SaveSharedPreference.setUserId(getApplicationContext(), response.id);
-                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                        startActivity(intent);
-                    }
-                });
+    private class SignUp implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (!validation.validate()) {
+                return;
             }
-        });
+
+            User attemptUser = userFromForm();
+            new Http(getApplicationContext()).post(HttpUrl.newUserUrl(), attemptUser, new HttpCallback<User>(User.class) {
+                @Override
+                public void onError(Error error) throws IOException {
+                    runOnUiThread(new RunnableToast(getApplicationContext(), error.toString()));
+                }
+
+                @Override
+                public void onSuccess(User response) throws IOException {
+                    SaveSharedPreference.setUserId(getApplicationContext(), response.id);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
