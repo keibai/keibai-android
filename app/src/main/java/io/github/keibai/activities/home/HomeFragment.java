4686e1c0cf808ab507538c908a60c10fb7d2997d
hzhu <zmark2013@gmail.com>
2018-01-02 19:49:16 +0100

    Home and Profile Layout modification

--- app/src/main/java/io/github/keibai/activities/home/HomeFragment.java
+++ app/src/main/java/io/github/keibai/activities/home/HomeFragment.java
@@ -8,17 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 import io.github.keibai.SaveSharedPreference;
 import io.github.keibai.activities.MainFragmentAbstract;
 import io.github.keibai.R;
+import io.github.keibai.activities.bid.BidAdapter;
+import io.github.keibai.activities.bid.BidLog;
+import io.github.keibai.activities.bid.WinnerAdapter;
 import io.github.keibai.activities.credit.CreditActivity;
 import io.github.keibai.http.Http;
 import io.github.keibai.http.HttpCallback;
 import io.github.keibai.http.HttpUrl;
+import io.github.keibai.models.Auction;
 import io.github.keibai.models.User;
 import io.github.keibai.models.meta.Error;
 
@@ -64,6 +70,8 @@ public class HomeFragment extends MainFragmentAbstract {
         super.onResume();
 
         fetchUser();
+        fetchAuctionList();
+        fetchBidList();
     }
 
     public void renderUser(User user) {
@@ -112,5 +120,79 @@ public class HomeFragment extends MainFragmentAbstract {
                 getActivity().runOnUiThread(new RunnableToast(getContext(), e.toString()));
             }
         });
+    }
+
+    private void fetchAuctionList() {
+        new Http(getContext()).get(HttpUrl.getAuctionListByWinnerId((int) SaveSharedPreference.getUserId(getContext())),
+                new HttpCallback<Auction[]>(Auction[].class) {
+                    @Override
+                    public void onError(Error error) throws IOException {
+                        getActivity().runOnUiThread(new RunnableToast(getContext(), error.toString()));
+                    }
+
+                    @Override
+                    public void onSuccess(final Auction[] response) throws IOException {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                renderAuction(response);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        getActivity().runOnUiThread(new RunnableToast(getContext(), e.toString()));
+                    }
+                });
+    }
+
+    private void renderAuction(Auction[] response) {
+        TextView participateActivity = view.findViewById(R.id.auction_won);
+
+        participateActivity.setText(String.valueOf(response.length));
+    }
+
+    private void fetchBidList() {
+        new Http(getContext()).get(HttpUrl.getBidListByOwnerId((int) SaveSharedPreference.getUserId(getContext())),
+                new HttpCallback<BidLog[]>(BidLog[].class) {
+                    @Override
+                    public void onError(Error error) throws IOException {
+                        getActivity().runOnUiThread(new RunnableToast(getContext(), error.toString()));
+                    }
+
+                    @Override
+                    public void onSuccess(final BidLog[] response) throws IOException {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                renderBid(response);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        getActivity().runOnUiThread(new RunnableToast(getContext(), e.toString()));
+                    }
+                });
+    }
+
+    private void renderBid(BidLog[] response) {
+        TextView madeBid = view.findViewById(R.id.made_bid);
+
+        madeBid.setText(String.valueOf(response.length));
+
+        double max = 0.0;
+
+        for (int i = 0; i < response.length; i++) {
+            if (response[i].getAmount() > max) {
+                max = response[i].getAmount();
+            }
+        }
+
+        TextView maxBid = view.findViewById(R.id.max_bid);
+
+        maxBid.setText(String.valueOf(max));
     }
 }
