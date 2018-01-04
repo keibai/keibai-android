bb5cec938eeb923726daa2ea5681f9f8a97cd1f7
zurfyx <zurfyx@gmail.com>
2018-01-04 20:00:33 +0100

    added status to BodyWS

--- app/src/main/java/io/github/keibai/models/meta/BodyWS.java
+++ app/src/main/java/io/github/keibai/models/meta/BodyWS.java
@@ -6,10 +6,11 @@ import java.util.regex.Pattern;
 public class BodyWS {
     public String type;
     public String nonce;
+    public int status = 200;
     public String json;
 
     public static BodyWS fromString(String text) {
-        Pattern pattern = Pattern.compile("(.*?),(.*?),(.*)");
+        Pattern pattern = Pattern.compile("(.*?),(.*?),([0-9]+),(.*)");
         Matcher matcher = pattern.matcher(text);
 
         if (!matcher.matches()) {
@@ -19,7 +20,8 @@ public class BodyWS {
         BodyWS body = new BodyWS();
         body.type = matcher.group(1);
         body.nonce = matcher.group(2);
-        body.json = matcher.group(3);
+        body.status = Integer.valueOf(matcher.group(3));
+        body.json = matcher.group(4);
 
         return body;
     }
@@ -32,6 +34,6 @@ public class BodyWS {
 
     @Override
     public String toString() {
-        return String.format("%s,%s,%s", type, nonce, json);
+        return String.format("%s,%s,%s,%s", type, nonce, status, json);
     }
 }
